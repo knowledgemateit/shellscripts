3224245fb9804b08999b6db88ee8bbc68e26ebe5
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2024-08-07 20:58:52 +0530

    Create packageisntall_servicestart.sh

---
+++ packageisntall_servicestart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read packagename
+package=$packagename
+sudo yum install $package -y
+echo " Please enter the name of service
+read servicename
+service_name=$servicename
+# Restart a specified service
+sudo systemctl restart "$service_name"
+echo "Service $service_name restarted."
