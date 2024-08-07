10c7f809b7b8575c5b47932d981631025f48517e
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2024-08-07 21:09:29 +0530

    Update 19.cpu-usage.sh

--- 19.cpu-usage.sh
+++ 19.cpu-usage.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cpuuse=$(cat /proc/loadavg | awk '{print $3}'|cut -f 1 -d ".")
+# cpuuse=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}' | cut -d. -f1)
 if [ "$cpuuse" -ge 90 ]; then
 echo "CPU current usage is: $cpuuse%" 
 else
