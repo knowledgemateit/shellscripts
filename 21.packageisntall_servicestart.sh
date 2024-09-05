80b6c2f86d914b20fd73f3839487ac4abe51a273
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2024-09-05 21:28:49 +0530

    Update 21.packageisntall_servicestart.sh

--- 21.packageisntall_servicestart.sh
+++ 21.packageisntall_servicestart.sh
@@ -7,5 +7,5 @@ echo " Please enter the name of service
 read servicename
 service_name=$servicename
 # Restart a specified service
-sudo systemctl restart "$service_name"
+sudo systemctl restart $service_name
 echo "Service $service_name restarted."
