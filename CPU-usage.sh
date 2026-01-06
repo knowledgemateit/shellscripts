9518e1da7c97e904e2be7796ac67181832634f54
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2026-01-06 07:54:01 +0530

    Delete CPU-usage.sh

--- CPU-usage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cpuuse=$(cat /proc/loadavg | awk '{print $3}'|cut -f 1 -d ".")
-# cpuuse=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}' | cut -d. -f1)
-if [ "$cpuuse" -ge 50 ]; then
-echo "CPU current usage is: $cpuuse%" 
-else
-echo "Server CPU usage is in under threshold"
-fi
