df73170b8a233d9a9d7bd99e619154461d64777a
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2026-01-06 07:56:28 +0530

    Update User_Account_Management_Script.sh

--- RealtimeusedScripts/User_Account_Management_Script.sh
+++ RealtimeusedScripts/User_Account_Management_Script.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-username="newuser"
-# Check if user exists; if not, create new user
-if id "$username" &>/dev/null; then
-echo "User $username already exists."
+# Script to add a user to Linux system
+if [ $(id -u) -eq 0 ]; then
+	read -p "Enter username : " username
+	read -s -p "Enter password : " password
+	egrep "^$username" /etc/passwd >/dev/null
+	if [ $? -eq 0 ]; then
+		echo "$username exists!"
+		exit 1
+	else
+		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+		useradd -m -p $pass $username
+		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+	fi
 else
-useradd -m "$username"
-echo "User $username created."
+	echo "Only root may add a user to the system"
+	exit 2
 fi
