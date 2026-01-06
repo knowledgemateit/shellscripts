2edb5c32826473ac04831ce9cc7fe8191dc4963c
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2026-01-06 07:54:14 +0530

    Delete Packageisntall.sh

--- Packageisntall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-packages=("git" "maven" "tree")
-# Install listed packages using yum
-for package in "${packages[@]}"; do
-sudo yum install "$package" -y
-done
-echo "Packages installed successfully"
